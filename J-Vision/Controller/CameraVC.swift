90b0f5b050331b8f89fdf0879cc34be9c6214991
Jeffrey Santana <santana@jefffreys-mini.attlocal.net>
2018-01-17 16:27:17 -0500

    setup camera to appear over the entire screen

--- J-Vision/Controller/CameraVC.swift
+++ J-Vision/Controller/CameraVC.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraVC: UIViewController {
 
+	//Outlets
 	@IBOutlet weak var captureImgView: UIImageView!
 	@IBOutlet weak var identificationLbl: UILabel!
 	@IBOutlet weak var confidenceLbl: UILabel!
@@ -17,10 +19,51 @@ class CameraVC: UIViewController {
 	@IBOutlet weak var flashBtn: UIButton!
 	@IBOutlet weak var roundedLblView: RoundedShadowView!
 	
+	//Variables
+	var captureSession: AVCaptureSession!			//work with camera
+	var cameraOutput: AVCapturePhotoOutput!			//take a still image
+	var previewLayer: AVCaptureVideoPreviewLayer!	//see through camera
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		captureSession = AVCaptureSession()
+		captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080	//capture full size of screen
+		
+		let backCamera = AVCaptureDevice.default(for: AVMediaType.video)	//video is bigger than photo
+		
+		do {
+			let input = try AVCaptureDeviceInput(device: backCamera!)
+			if captureSession.canAddInput(input) {
+				captureSession.addInput(input)
+			}
+			
+			cameraOutput = AVCapturePhotoOutput()
+			if captureSession.canAddOutput(cameraOutput) {
+				captureSession.addOutput(cameraOutput!)
+				
+				previewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+				previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+				previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+				
+				cameraView.layer.addSublayer(previewLayer!)
+				captureSession.startRunning()
+			}
+		} catch {
+			debugPrint(error)
+		}
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		previewLayer.frame = cameraView.bounds		//same size as cameraView
 	}
 
 }
 
+extension CameraVC {
+	
+}
